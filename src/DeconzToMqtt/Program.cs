293177aae66ba1f2d23846ae7c480a4e22fbec80
Rick van Buuren <rvanbuuren@vitas.nl>
2020-12-04 11:06:44 +0100

    first draft websockets

---
+++ src/DeconzToMqtt/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeconzToMqtt
+{
+    internal class Program
+    {
+        private static readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private static async Task Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true))
+                .AddSingleton<IWebSocketService, WebSocketService>()
+                .BuildServiceProvider();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+
+            Log.Information("Starting application...");
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                _cancellationTokenSource.Cancel();
+                Console.WriteLine("Exiting...");
+                Environment.Exit(0);
+            };
+
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            // run websocket task in background
+            var websocketService = serviceProvider.GetService<IWebSocketService>();
+            var websocketTask = Task.Run(() => websocketService.StartAsync(cancellationToken), cancellationToken);
+
+            Log.Information("Press any key to stop application...");
+            Task.WaitAll(websocketTask);
+        }
+
+        private static IConfiguration LoadConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
---
+++ src/DeconzToMqtt/WebSocketService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeconzToMqtt
+{
+    public interface IWebSocketService
+    {
+        Task StartAsync(CancellationToken token);
+    }
+
+    // https://thecodegarden.net/websocket-client-dotnet
+    public class WebSocketService : IWebSocketService
+    {
+        private readonly ILogger<WebSocketService> _logger;
+
+        public WebSocketService(ILogger<WebSocketService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken token)
+        {
+            do
+            {
+                using var socket = new ClientWebSocket();
+                try
+                {
+                    await socket.ConnectAsync(new Uri("ws://192.168.0.93:8443"), token);
+                    await ReceiveAsync(socket, token);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Websocket problem.");
+                    throw;
+                }
+            } while (!token.IsCancellationRequested);
+        }
+
+        private async Task ReceiveAsync(ClientWebSocket socket, CancellationToken token)
+        {
+            var buffer = new ArraySegment<byte>(new byte[2048]);
+            do
+            {
+                WebSocketReceiveResult result;
+                using var ms = new MemoryStream();
+                do
+                {
+                    result = await socket.ReceiveAsync(buffer, token);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                } while (!result.EndOfMessage);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                    break;
+
+                ms.Seek(0, SeekOrigin.Begin);
+                using var reader = new StreamReader(ms, Encoding.UTF8);
+                _logger.LogInformation(await reader.ReadToEndAsync());
+            } while (!token.IsCancellationRequested);
+        }
+    }
+}
