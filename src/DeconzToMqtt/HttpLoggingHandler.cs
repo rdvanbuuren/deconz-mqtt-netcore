813f2740ce8cc64d6a89e3ca731433a734f1436c
Rick van Buuren <rvanbuuren@vitas.nl>
2020-12-07 10:38:41 +0100

    refactored HttpLoggingHandler

--- src/DeconzToMqtt/HttpLoggingHandler.cs
+++ src/DeconzToMqtt/HttpLoggingHandler.cs
@@ -12,6 +12,9 @@ namespace DeconzToMqtt
     /// <summary>
     /// Logs http client request/response.
     /// </summary>
+    /// <remarks>
+    /// Credits: https://github.com/reactiveui/refit/issues/258#issuecomment-243394076
+    /// </remarks>
     public class HttpLoggingHandler : DelegatingHandler
     {
         private readonly string[] _types = new[] { "html", "text", "xml", "json", "txt", "x-www-form-urlencoded" };
@@ -22,27 +25,26 @@ namespace DeconzToMqtt
             _logger = logger;
         }
 
-        async protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var req = request;
             var id = Guid.NewGuid().ToString();
-            var msg = $"[{id} -   Request]";
+            var msg = $"[{id} - Request]";
 
             _logger.LogDebug($"{msg}========Start==========");
-            _logger.LogDebug($"{msg} {req.Method} {req.RequestUri.PathAndQuery} {req.RequestUri.Scheme}/{req.Version}");
-            _logger.LogDebug($"{msg} Host: {req.RequestUri.Scheme}://{req.RequestUri.Host}");
+            _logger.LogDebug($"{msg} {request.Method} {request.RequestUri.PathAndQuery} {request.RequestUri.Scheme}/{request.Version}");
+            _logger.LogDebug($"{msg} Host: {request.RequestUri.Scheme}://{request.RequestUri.Host}");
 
-            foreach (var header in req.Headers)
+            foreach (var header in request.Headers)
                 _logger.LogDebug($"{msg} {header.Key}: {string.Join(", ", header.Value)}");
 
-            if (req.Content != null)
+            if (request.Content != null)
             {
-                foreach (var header in req.Content.Headers)
+                foreach (var header in request.Content.Headers)
                     _logger.LogDebug($"{msg} {header.Key}: {string.Join(", ", header.Value)}");
 
-                if (req.Content is StringContent || this.IsTextBasedContentType(req.Headers) || this.IsTextBasedContentType(req.Content.Headers))
+                if (request.Content is StringContent || IsTextBasedContentType(request.Headers) || IsTextBasedContentType(request.Content.Headers))
                 {
-                    var result = await req.Content.ReadAsStringAsync();
+                    var result = await request.Content.ReadAsStringAsync();
 
                     _logger.LogDebug($"{msg} Content:");
                     _logger.LogDebug($"{msg} {string.Join("", result.Cast<char>().Take(255))}");
@@ -50,9 +52,7 @@ namespace DeconzToMqtt
             }
 
             var start = DateTime.Now;
-
-            var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
-
+            var response = await base.SendAsync(request, cancellationToken);
             var end = DateTime.Now;
 
             _logger.LogDebug($"{msg} Duration: {end - start}");
@@ -61,26 +61,24 @@ namespace DeconzToMqtt
             msg = $"[{id} - Response]";
             _logger.LogDebug($"{msg}=========Start=========");
 
-            var resp = response;
+            _logger.LogDebug($"{msg} {request.RequestUri.Scheme.ToUpper()}/{response.Version} {(int)response.StatusCode} {response.ReasonPhrase}");
 
-            _logger.LogDebug($"{msg} {req.RequestUri.Scheme.ToUpper()}/{resp.Version} {(int)resp.StatusCode} {resp.ReasonPhrase}");
-
-            foreach (var header in resp.Headers)
+            foreach (var header in response.Headers)
                 _logger.LogDebug($"{msg} {header.Key}: {string.Join(", ", header.Value)}");
 
-            if (resp.Content != null)
+            if (response.Content != null)
             {
-                foreach (var header in resp.Content.Headers)
+                foreach (var header in response.Content.Headers)
                     _logger.LogDebug($"{msg} {header.Key}: {string.Join(", ", header.Value)}");
 
-                if (resp.Content is StringContent || this.IsTextBasedContentType(resp.Headers) || this.IsTextBasedContentType(resp.Content.Headers))
+                if (response.Content is StringContent || IsTextBasedContentType(response.Headers) || IsTextBasedContentType(response.Content.Headers))
                 {
                     start = DateTime.Now;
-                    var result = await resp.Content.ReadAsStringAsync();
+                    var result = await response.Content.ReadAsStringAsync();
                     end = DateTime.Now;
 
                     _logger.LogDebug($"{msg} Content:");
-                    _logger.LogDebug($"{msg} {string.Join("", result.Cast<char>().Take(255))}...");
+                    _logger.LogDebug($"{msg} {string.Join("", result.Cast<char>().Take(255))}");
                     _logger.LogDebug($"{msg} Duration: {end - start}");
                 }
             }
