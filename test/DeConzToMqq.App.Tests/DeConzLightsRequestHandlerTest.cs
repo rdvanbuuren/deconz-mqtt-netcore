c93e8d964a09010c2e59dfadfe2efd67e88d7609
Rick van Buuren <rvanbuuren@vitas.nl>
2020-12-08 19:50:17 +0100

    moved files and added unit tests

---
+++ test/DeConzToMqq.App.Tests/DeConzLightsRequestHandlerTest.cs
@@ -0,0 +1,40 @@
+using AutoFixture;
+using DeConzToMqtt.App.Handlers;
+using DeConzToMqtt.Domain.DeConz;
+using DeConzToMqtt.Domain.DeConz.Apis;
+using DeConzToMqtt.Domain.DeConz.Dtos.Lights;
+using DeConzToMqtt.Domain.DeConz.Requests;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeConzToMqq.App.Tests
+{
+    public class DeConzLightsRequestHandlerTest
+    {
+        private readonly IFixture _fixture = new Fixture();
+
+        [Fact]
+        public async Task Handle_Should_Call_Api()
+        {
+            // arrange
+            var options = _fixture.Create<DeConzOptions>();
+            var lights = _fixture.Create<IDictionary<string, Light>>();
+
+            var mockApi = new Mock<IDeConzLightsApi>(MockBehavior.Strict);
+            mockApi.Setup(api => api.GetLightsAsync(options.ApiKey, default)).ReturnsAsync(lights);
+
+            var handler = new DeConzLightsRequestHandler(mockApi.Object, Options.Create(options), new NullLogger<DeConzLightsRequestHandler>());
+
+            // act
+            var result = await handler.Handle(new DeConzLightsRequest(), default);
+
+            // assert
+            result.Should().BeEquivalentTo(lights.Values);
+        }
+    }
+}
