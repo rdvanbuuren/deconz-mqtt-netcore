23fd4917fed90851eb3a9918e073b43ce28dc75d
Rick van Buuren <rvanbuuren@vitas.nl>
2020-12-09 20:09:05 +0100

    dispose of client

--- src/DeconzToMqtt.App/DeConz/WebsocketService.cs
+++ src/DeconzToMqtt.App/DeConz/WebsocketService.cs
@@ -14,12 +14,18 @@ using Websocket.Client;
 
 namespace DeConzToMqtt.App.DeConz
 {
-    public class WebsocketService : IHostedService
+    /// <summary>
+    /// Websocket service that listens for incoming messages from deCONZ.
+    /// </summary>
+    public class WebsocketService : IHostedService, IDisposable
     {
         private readonly IWebsocketClientFactory _clientFactory;
         private readonly IMediator _mediator;
         private readonly ILogger<WebsocketService> _logger;
         private readonly DeConzOptions _options;
+
+        private IWebsocketClient _client;
+        private bool isDisposed;
 
         /// <summary>
         /// Creates a new instance of the <see cref="WebsocketService"/> class.
@@ -36,55 +42,73 @@ namespace DeConzToMqtt.App.DeConz
             _logger = logger;
         }
 
+        /// <inheritdoc/>
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             var websocketPort = await _mediator.Send(new DeConzWebsocketRequest(), cancellationToken);
             var url = new Uri($"ws://{_options.Host}:{websocketPort}");
 
-            var client = _clientFactory.CreateClient(url);
+            _client = _clientFactory.CreateClient(url);
 
-            client.Name = "deCONZ";
-            client.ReconnectTimeout = TimeSpan.FromMinutes(5);
-            client.ErrorReconnectTimeout = TimeSpan.FromSeconds(30);
+            _client.Name = "deCONZ";
+            _client.ReconnectTimeout = TimeSpan.FromMinutes(5);
+            _client.ErrorReconnectTimeout = TimeSpan.FromSeconds(30);
 
-            client.ReconnectionHappened.Subscribe(info =>
+            _client.ReconnectionHappened.Subscribe(info =>
             {
                 if (info.Type == ReconnectionType.Initial)
                 {
-                    _logger.LogInformation($"Connecting to websocket: {client.Url}");
+                    _logger.LogInformation($"Connecting to websocket: {_client.Url}");
                 }
                 else
                 {
-                    _logger.LogInformation($"Reconnecting to websocket: {client.Url} - cause: {info.Type}");
+                    _logger.LogInformation($"Reconnecting to websocket: {_client.Url} - cause: {info.Type}");
                 }
             });
 
-            client.DisconnectionHappened.Subscribe(info =>
+            _client.DisconnectionHappened.Subscribe(info =>
             {
                 _logger.LogWarning($"Websocket got disconnected - cause: {info.Type}");
             });
 
-            client.MessageReceived.Subscribe(async msg =>
+            _client.MessageReceived.Subscribe(async msg =>
             {
-                await MessageReceivedAsync(msg, cancellationToken);
+                _logger.LogInformation($"Message received: {msg}");
+                var message = JsonConvert.DeserializeObject<Message>(msg.Text);
+
+                // emit event so mqtt service will pick it up.
+                await _mediator.Publish(new DeConzMessageEvent(message), cancellationToken);
             });
 
             _logger.LogInformation("Starting websocket client...");
-            await client.Start();
+            await _client.Start();
         }
 
+        /// <inheritdoc/>
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            return Task.CompletedTask;
+            return _client.Stop(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, "WebsocketService stopped.");
         }
 
-        private async Task MessageReceivedAsync(ResponseMessage message, CancellationToken cancellationToken)
+        protected virtual void Dispose(bool disposing)
         {
-            _logger.LogInformation($"Message received: {message}");
-            var msg = JsonConvert.DeserializeObject<Message>(message.Text);
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    _client?.Dispose();
+                    _client = null;
+                }
 
-            // emit event so mqtt service will pick it up.
-            await _mediator.Publish(new DeConzMessageEvent(msg), cancellationToken);
+                isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
