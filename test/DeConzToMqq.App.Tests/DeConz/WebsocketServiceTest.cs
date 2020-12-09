a462d24358bf485b442b758a22e0f21ea76297a3
Rick van Buuren <rvanbuuren@vitas.nl>
2020-12-09 20:09:57 +0100

    added unit test for MessageReveived observable

--- test/DeConzToMqq.App.Tests/DeConz/WebsocketServiceTest.cs
+++ test/DeConzToMqq.App.Tests/DeConz/WebsocketServiceTest.cs
@@ -1,12 +1,15 @@
 ﻿using AutoFixture;
 using DeConzToMqtt.App.DeConz;
 using DeConzToMqtt.Domain.DeConz;
+using DeConzToMqtt.Domain.DeConz.Dtos.WebSocket;
+using DeConzToMqtt.Domain.DeConz.Events;
 using DeConzToMqtt.Domain.DeConz.Requests;
 using FluentAssertions;
 using MediatR;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
 using Websocket.Client;
@@ -25,14 +28,13 @@ namespace DeConzToMqq.App.Tests.DeConz
             var options = _fixture.Create<DeConzOptions>();
             var websocketPort = _fixture.Create<int>();
 
-            var disposable = new Mock<IDisposable>();
-
+            var disposableMock = new Mock<IDisposable>();
             var reconnectionHappenedMock = new Mock<IObservable<ReconnectionInfo>>(MockBehavior.Strict);
-            reconnectionHappenedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<ReconnectionInfo>>())).Returns(disposable.Object).Verifiable();
+            reconnectionHappenedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<ReconnectionInfo>>())).Returns(disposableMock.Object).Verifiable();
             var disconnectionHappenedMock = new Mock<IObservable<DisconnectionInfo>>(MockBehavior.Strict);
-            disconnectionHappenedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<DisconnectionInfo>>())).Returns(disposable.Object).Verifiable();
+            disconnectionHappenedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<DisconnectionInfo>>())).Returns(disposableMock.Object).Verifiable();
             var messageReceivedMock = new Mock<IObservable<ResponseMessage>>(MockBehavior.Strict);
-            messageReceivedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<ResponseMessage>>())).Returns(disposable.Object).Verifiable();
+            messageReceivedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<ResponseMessage>>())).Returns(disposableMock.Object).Verifiable();
 
             var clientMock = new Mock<IWebsocketClient>(MockBehavior.Strict);
             clientMock.SetupSet(c => c.Name = It.IsAny<string>()).Callback<string>(value => value.Should().Be("deCONZ"));
@@ -61,6 +63,50 @@ namespace DeConzToMqq.App.Tests.DeConz
             disconnectionHappenedMock.Verify(o => o.Subscribe(It.IsAny<IObserver<DisconnectionInfo>>()), Times.Once, "Needs one subsccription.");
             messageReceivedMock.Verify(o => o.Subscribe(It.IsAny<IObserver<ResponseMessage>>()), Times.Once, "Needs one subsccription.");
             clientMock.Verify(c => c.Start(), Times.Once, "Websocket client should be started.");
+        }
+
+        [Fact]
+        public async Task StartAsync_Should_ReceiveMessage_And_Publish()
+        {
+            var options = _fixture.Create<DeConzOptions>();
+            var websocketPort = _fixture.Create<int>();
+            var message = _fixture.Create<Message>();
+            var messageJson = JsonConvert.SerializeObject(message);
+
+            var disposable = new Mock<IDisposable>();
+            var reconnectionHappenedMock = new Mock<IObservable<ReconnectionInfo>>();
+            var disconnectionHappenedMock = new Mock<IObservable<DisconnectionInfo>>();
+            var messageReceivedMock = new Mock<IObservable<ResponseMessage>>(MockBehavior.Strict);
+            messageReceivedMock.Setup(o => o.Subscribe(It.IsAny<IObserver<ResponseMessage>>()))
+                .Callback((IObserver<ResponseMessage> observer) =>
+                {
+                    observer.OnNext(ResponseMessage.TextMessage(messageJson));
+                })
+                .Returns(disposable.Object);
+
+            var clientMock = new Mock<IWebsocketClient>(MockBehavior.Strict);
+            clientMock.SetupSet(c => c.Name = It.IsAny<string>());
+            clientMock.SetupSet(c => c.ReconnectTimeout = It.IsAny<TimeSpan>());
+            clientMock.SetupSet(c => c.ErrorReconnectTimeout = It.IsAny<TimeSpan>());
+            clientMock.SetupGet(c => c.ReconnectionHappened).Returns(reconnectionHappenedMock.Object);
+            clientMock.SetupGet(c => c.DisconnectionHappened).Returns(disconnectionHappenedMock.Object);
+            clientMock.SetupGet(c => c.MessageReceived).Returns(messageReceivedMock.Object);
+            clientMock.Setup(c => c.Start()).Returns(Task.CompletedTask);
+
+            var clientFactoryMock = new Mock<IWebsocketClientFactory>(MockBehavior.Strict);
+            clientFactoryMock.Setup(f => f.CreateClient(It.IsAny<Uri>())).Returns(clientMock.Object);
+
+            var mediatorMock = new Mock<IMediator>(MockBehavior.Strict);
+            mediatorMock.Setup(m => m.Send(It.IsAny<DeConzWebsocketRequest>(), default)).ReturnsAsync(websocketPort);
+            mediatorMock.Setup(m => m.Publish(It.IsAny<DeConzMessageEvent>(), default)).Returns(Task.CompletedTask).Verifiable();
+
+            var service = new WebsocketService(clientFactoryMock.Object, mediatorMock.Object, Options.Create(options), new NullLogger<WebsocketService>());
+
+            // act
+            await service.StartAsync(default);
+
+            // assert
+            mediatorMock.Verify(m => m.Publish(It.IsAny<DeConzMessageEvent>(), default), Times.Once, "Needs to be called once.");
         }
     }
 }
